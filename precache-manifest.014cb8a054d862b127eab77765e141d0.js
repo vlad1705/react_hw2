4f67ad9c98e02a199d6fe157fc4823830e46d41d
Vladyslav Pasemko <vpasemko@gmail.com>
2020-08-13 21:24:11 +0300

    Updates

---
+++ precache-manifest.014cb8a054d862b127eab77765e141d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5715fd724066c8f6d3f2ce73af393f6f",
+    "url": "/react_hw2/index.html"
+  },
+  {
+    "revision": "fed45e48075d85c623bd",
+    "url": "/react_hw2/static/css/main.e38a604e.chunk.css"
+  },
+  {
+    "revision": "27b1fc9c59f777f9ff50",
+    "url": "/react_hw2/static/js/2.eaec67fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_hw2/static/js/2.eaec67fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed45e48075d85c623bd",
+    "url": "/react_hw2/static/js/main.53ce59a9.chunk.js"
+  },
+  {
+    "revision": "7ab3977848245cccdef3",
+    "url": "/react_hw2/static/js/runtime-main.826a49a2.js"
+  }
+]);
